c2b78f28448bbc6777a533b3ac076c3adfc11bb0
Mershelle Rivera <mershellemae@gmail.com>
2022-08-05 10:09:21 -0700

    Inheritance Demo

---
+++ src/inheritancedemo/Hospital.java
@@ -0,0 +1,9 @@
+package inheritancedemo;
+
+public class Hospital {
+    public static void main(String args[]) {
+        Surgeon s = new Surgeon();
+        s.Doctor_Details();
+        s.Surgeon_Details();
+    }
+}

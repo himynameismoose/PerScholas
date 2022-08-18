f0cb9ec1453b39f2b212fb09c4fda1448a4d24fc
Mershelle Rivera <mershellemae@gmail.com>
2022-08-18 15:42:03 -0700

    Create a JDBC connection to MySQL db

---
+++ src/jdbc/JDBCConnection.java
@@ -0,0 +1,25 @@
+package jdbc;
+
+import java.sql.*;
+
+public class JDBCConnection {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_class", "root", "password");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from class");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}

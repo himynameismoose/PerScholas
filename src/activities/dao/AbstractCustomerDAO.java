83b8ce9e17297a49fdcb670b25c00dacdd501c10
Mershelle Rivera <mershellemae@gmail.com>
2022-08-19 11:16:51 -0700

    abstract class

---
+++ src/activities/dao/AbstractCustomerDAO.java
@@ -0,0 +1,39 @@
+package activities.dao;
+
+import java.sql.*;
+
+public class AbstractCustomerDAO {
+    private final String URL =  "jdbc:mysql://localhost:3306/classicmodels";
+    private final String USER = "root";
+    private final String PASS = "password";
+    protected Connection conn=null;
+    protected PreparedStatement ps = null;
+    protected ResultSet rs = null;
+
+    // method to connect to the database
+    public void connect(){
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (!rs.equals(null)) {
+                if (!rs.isClosed()) rs.close();
+            }
+
+            if (!ps.equals(null)) {
+                if(!ps.isClosed()) ps.close();
+            }
+
+            if (!conn.equals(null)) {
+                if(!conn.isClosed()) conn.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}

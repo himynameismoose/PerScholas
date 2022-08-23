77658501f0fc4361077e201d3efdfd679b31f854
Mershelle Rivera <mershellemae@gmail.com>
2022-08-23 16:59:24 -0700

    class to process message

---
+++ src/assignments/multithreading/Notifier.java
@@ -0,0 +1,27 @@
+package assignments.multithreading;
+
+public class Notifier implements Runnable {
+
+    private Message message;
+
+    public Notifier(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " started");
+
+        try {
+            Thread.sleep(1000);
+
+            synchronized (message) {
+                message.setMessage(name + " Notifier work done");
+                message.notify();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
